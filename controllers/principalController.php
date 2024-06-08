110c35aa9fc554bd3db37b8781b960d49bd38c32
ztrackperu <ztrack@zgroup.com.pe>
2024-06-08 10:32:51 -0500

    reparar genset 10

--- controllers/principalController.php
+++ controllers/principalController.php
@@ -709,7 +709,7 @@ if($document['ethylene']>230){
         $empresa =$contenedor['empresa_id'];
         $data['empresa'] = $principal->empresaAsociada($empresa);
 
-        echo json_encode($data);      
+        echo json_encode($data['punto']);      
         break;
 
 
