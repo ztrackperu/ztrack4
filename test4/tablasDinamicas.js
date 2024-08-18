d3fea950bbc3d1c3af28876bb23da1ae5caa5688
ztrackperu <ztrack@zgroup.com.pe>
2024-08-18 08:11:16 -0500

    panel para jhon vena 6

--- test4/tablasDinamicas.js
+++ test4/tablasDinamicas.js
@@ -33,7 +33,8 @@ async function terrible_f(){
                 const celsius = (SP_Setpoint - 32) * 5 / 9;
     
                 // Redondear al entero más cercano y retornar el resultado
-                fato_f = Math.round(celsius);
+                //fato_f = Math.round(celsius);
+                fato_f =parseInt(celsius, 10);
                 console.log(fato_f)
                 /*
                 const config = {
