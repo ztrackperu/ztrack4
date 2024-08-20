b4a968757c061a491b4ff9f6dcec1a3d397c82d3
ztrackperu <ztrack@zgroup.com.pe>
2024-08-20 07:45:27 -0500

    base ususarios salog 16

--- test4/indexs.php
+++ test4/indexs.php
@@ -4,4 +4,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start(); 
 echo" ola mundo ";
+
+//$tipo_usuario = 2 ;
+$tipo_usuario =$_SESSION['permiso'] ;
+$usuario = $_SESSION['id'] ;
+echo "ola mundo";
+echo " este es el  TIPO usuario : ".$tipo_usuario ;
+echo " este es el USUARIO : ".$usuario ;
 ?>
