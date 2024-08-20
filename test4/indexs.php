fd06e1fc7986d67965fd99ac47912f4d5f4379fa
ztrackperu <ztrack@zgroup.com.pe>
2024-08-20 07:46:33 -0500

    base ususarios salog 17

--- test4/indexs.php
+++ test4/indexs.php
@@ -1,4 +1,6 @@
 <?php
+require_once '../models/integral.php';
+$api = new IntegralModel();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -11,4 +13,12 @@ $usuario = $_SESSION['id'] ;
 echo "ola mundo";
 echo " este es el  TIPO usuario : ".$tipo_usuario ;
 echo " este es el USUARIO : ".$usuario ;
+if($tipo_usuario ==1){
+    $empresa_general = 1;
+  }else {
+    echo " este es el usuario : ".$usuario ;
+    $datosUsuario = $api->UsuarioEmpresa($usuario);
+    echo var_dump($datosUsuario);
+    $empresa_general = $datosUsuario['empresa_id'] ;
+  }
 ?>
