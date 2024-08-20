8955996e5920981be55ca76c7248d79c7b0506de
ztrackperu <ztrack@zgroup.com.pe>
2024-08-20 09:15:38 -0500

    base ususarios salog 41

--- user.php
+++ user.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'config.php';
 require_once 'controllers/plantillaController.php';
 $plantilla = new Plantilla();
